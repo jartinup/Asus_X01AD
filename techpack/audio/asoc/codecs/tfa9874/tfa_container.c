74269359990e76597582b99af8760da1ea9b14eb
shashi918 <sr0298037@gmail.com>
2021-01-05 10:24:37 +0530

    TFA v6.5.5

--- techpack/audio/asoc/codecs/tfa9874/tfa_container.c
+++ techpack/audio/asoc/codecs/tfa9874/tfa_container.c
@@ -1,17 +1,10 @@
 /*
- * Copyright 2014-2017 NXP Semiconductors
+ * Copyright (C) 2014 NXP Semiconductors, All Rights Reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
  *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 #include "dbgprint.h"
@@ -679,12 +672,6 @@ enum Tfa98xx_Error tfaContWriteFile(struct tfa_device *tfa,  nxpTfaFileDsc_t *fi
 		} else {
 			err = tfaContWriteVstep(tfa, (nxpTfaVolumeStep2File_t *)hdr, vstep_idx);
 		}
-
-		/* If writing the vstep was succesfull, set new current vstep */
-		if(err == Tfa98xx_Error_Ok) {
-			tfa->vstep = vstep_idx;
-		}
-
 		break;
 	case speakerHdr:
 		if (tfa->tfa_family == 2) {
